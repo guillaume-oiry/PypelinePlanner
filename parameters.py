99b55af74eb97f5101d694a727fd61443fe4d04e
Guillaume <guillaume_oiry@protonmail.com>
2025-11-14 12:47:21 +0100

    Correct parameters example.

--- parameters.py
+++ parameters.py
@@ -1,11 +1,11 @@
-PARAMETERS = {'preprocessing' : {'sart' : {'CONDITION' : lambda file_path : 'task-sart' in file_path,
+PARAMETERS = {'preprocessing' : {'all' : {'CONDITION' : lambda file_path : 'task-sart' in file_path,
                                            'PARAMETERS' : {'EPICE_minimal_preprocessing' : {'LOW_FREQ' : 0.1,
                                                                                             'HIGH_FREQ' : 40,
                                                                                             'NOTCH_FILTER' : 50}}},
-                                 'other' : {'CONDITION' : lambda file_path : True,
+                                 'other' : {'CONDITION' : lambda file_path : 'task-sart' not in file_path,
                                             'PARAMETERS' : {'EPICE_minimal_preprocessing' : {'LOW_FREQ' : 0.1,
                                                                                             'HIGH_FREQ' : 40,
-                                                                                            'NOTCH_FILTER' : 50}}}},
+                                                                                            'NOTCH_FILTER' : 50}}}}
               'processing' : {'extraction' : {'all' : {'CONDITIONS' : lambda info : True,
                                                        'PARAMETERS' : {'subset_epoching' : {'tmin' : -20,
                                                                                             'tmax' : 0,
@@ -16,15 +16,13 @@ PARAMETERS = {'preprocessing' : {'sart' : {'CONDITION' : lambda file_path : 'tas
                                                                                             }
                                                                        }
                                                        }
-                                              }
-                              },
-              'postprocessing' : {'scheme1' : {'views' : {'func1' : {}},
-                                               'statistics' : {'func1' : {},
-                                                               'func2' : {}},
-                                               'figures' : {'func1' : {},
-                                                            'func2' : {},
-                                                            'func3' : {}
-                                                            }
-                                               }
+                                              },
+                              'analysis' : {'epochs' : {'CONDITIONS' : lambda info : 'epochs' in info['extraction'],
+                                                        'PARAMETERS' : {'psd_data' : {'bp' : False}
+                                                                        }
+                                                        }
+                                            }
+                              }
+              'postprocessing' : {'psd_mean' : {}
                                   }
               }
