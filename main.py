ac46ff91e124f113b14d9ab18086afd9fdbe79e4
Guillaume <guillaume_oiry@protonmail.com>
2025-11-19 12:25:31 +0100

    Improved lisibility of view.py and formatted python code with black.

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 import modules
-from parameters import data_query, PARAMETERS
-import glob
-import warnings
-import copy
-import multiprocessing
-import time
+from parameters import DATA_QUERY, PARAMETERS
 
-def main(file_path_list, parameters, apply_multiprocessing = True):
+import glob
+import copy
+import warnings
+import multiprocessing
+
+
+def main(file_path_list, parameters, apply_multiprocessing=True):
     if apply_multiprocessing:
         preprocessing_dict = preprocessing_mp(file_path_list, parameters)
-        processing_dict = processing_mp(preprocessing_dict, parameters)
-        postprocessing_dict = postprocessing_mp(copy.deepcopy(processing_dict), parameters)
+        processing_dict = processing_mp(copy.deepcopy(preprocessing_dict), parameters)
+        postprocessing_dict = postprocessing_mp(
+            copy.deepcopy(processing_dict), parameters
+        )
     else:
         preprocessing_dict = preprocessing(file_path_list, parameters)
-        processing_dict = processing(preprocessing_dict, parameters)
-        postprocessing_dict = postprocessing_mp(copy.deepcopy(processing_dict), parameters)
-    return postprocessing_dict
+        processing_dict = processing(copy.deepcopy(preprocessing_dict), parameters)
+        postprocessing_dict = postprocessing_mp(
+            copy.deepcopy(processing_dict), parameters
+        )
+    return preprocessing_dict, processing_dict, postprocessing_dict
+
+
+# %% PREPROCESSING STEP
+
 
 def preprocessing(file_path_list, parameters):
 
     preprocessing_dict = {}
 
     for file_path in file_path_list:
-        functions = get_functions_with_args(file_path,
-                                              parameters = parameters['preprocessing'],
-                                              step = 'preprocessing',
-                                              step_module = modules.preprocess)
-        function, kwargs = functions[0] # In the preprocessing step there is only one function by file.
-        data = function(file_path, parameters = kwargs, info=file_path)
-        preprocessing_dict[file_path] = nest_data(list(parameters['processing'].keys()), data) # Put the data at the bottom of the processing tree.
+        functions = get_functions_with_args(
+            file_path,
+            parameters=parameters["preprocessing"],
+            step="preprocessing",
+            step_module=modules.preprocess,
+        )
+
+        # In the preprocessing step there is only one preprocessing function allowed by file so we take only the first item.
+        function, kwargs = functions[0]
+
+        data = function(file_path, parameters=kwargs, info=file_path)
+        preprocessing_dict[file_path] = data
 
     return preprocessing_dict
+
 
 def preprocessing_mp(file_path_list, parameters):
 
@@ -40,75 +55,104 @@ def preprocessing_mp(file_path_list, parameters):
     mp_list = []
 
     for file_path in file_path_list:
-        functions = get_functions_with_args(file_path,
-                                        parameters = parameters['preprocessing'],
-                                        step = 'preprocessing',
-                                        step_module = modules.preprocess)
-        function, kwargs = functions[0] # In the preprocessing step there is only one function by file.
+        functions = get_functions_with_args(
+            file_path,
+            parameters=parameters["preprocessing"],
+            step="preprocessing",
+            step_module=modules.preprocess,
+        )
 
-        process = multiprocessing.Process(target=preprocess_mp, args=(q, file_path, function, kwargs))
+        # In the preprocessing step there is only one preprocessing function allowed by file so we take only the first item.
+        function, kwargs = functions[0]
+
+        process = multiprocessing.Process(
+            target=preprocess_mp, args=(q, file_path, function, kwargs)
+        )
         process.start()
         mp_list.append(process)
 
     for _ in mp_list:
         file_path, data = q.get()
-        preprocessing_dict[file_path] = nest_data(list(parameters["processing"].keys()), data) # Put the data at the bottom of the processing tree.
+        preprocessing_dict[file_path] = data
 
     for p in mp_list:
         p.join()
-        #p.close()
 
     return preprocessing_dict
 
+
 def preprocess_mp(q, file_path, function, kwargs):
-    data = function(file_path = file_path, parameters = kwargs, info = file_path)
+    data = function(file_path=file_path, parameters=kwargs, info=file_path)
     q.put((file_path, data))
+
+
+# %% PROCESSING STEP
+
 
 def processing(preprocessing_dict, parameters):
 
+    initial_processing_dict = preprocessing_dict_to_initial_processing_dict(
+        preprocessing_dict, parameters
+    )
     processing_dict = {}
 
-    for file_path, empty_tree in preprocessing_dict.items():
-        individual_dict = rec_processing(individual_dict = {file_path : empty_tree},
-                                         parameters = parameters,
-                                         steps = list(parameters['processing'].keys())
-                                         )
-        for path, data_tree in individual_dict.items(): # Logically, there is only one set of key-value : the file_path and the corresponding processing tree.
-            processing_dict[path] = data_tree
+    for file_path, empty_tree in initial_processing_dict.items():
+        individual_dict = rec_processing(
+            individual_dict={file_path: empty_tree},
+            parameters=parameters,
+            steps=list(parameters["processing"].keys()),
+        )
+        processing_dict.update(individual_dict)
 
     return processing_dict
 
+
 def processing_mp(preprocessing_dict, parameters):
 
+    initial_processing_dict = preprocessing_dict_to_initial_processing_dict(
+        preprocessing_dict, parameters
+    )
     processing_dict = {}
 
     q = multiprocessing.Queue()
     mp_list = []
 
-    for file_path, empty_tree in preprocessing_dict.items():
-        process = multiprocessing.Process(target=process_mp, args=(q, file_path, empty_tree, parameters))
+    for file_path, empty_tree in initial_processing_dict.items():
+        process = multiprocessing.Process(
+            target=process_mp, args=(q, file_path, empty_tree, parameters)
+        )
         process.start()
         mp_list.append(process)
 
     for _ in mp_list:
         individual_dict = q.get()
-        for path, data_tree in individual_dict.items(): # Logically, there is only one set of key-value : the file_path and the corresponding processing tree.
-            processing_dict[path] = data_tree
+        processing_dict.update(individual_dict)
 
     for p in mp_list:
         p.join()
-        #p.close()
 
     return processing_dict
 
+
 def process_mp(q, file_path, empty_tree, parameters):
-    individual_dict = rec_processing(individual_dict = {file_path : empty_tree},
-                                     parameters = parameters,
-                                     steps = list(parameters['processing'].keys())
-                                     )
+    individual_dict = rec_processing(
+        individual_dict={file_path: empty_tree},
+        parameters=parameters,
+        steps=list(parameters["processing"].keys()),
+    )
     q.put(individual_dict)
 
-def rec_processing(individual_dict, parameters, steps, nstep = 0, info = {}):
+
+def preprocessing_dict_to_initial_processing_dict(preprocessing_dict, parameters):
+    initial_processing_dict = {}
+    for file_path, data in preprocessing_dict.items():
+        initial_processing_dict[file_path] = nest_data(
+            list(parameters["processing"].keys()), data
+        )  # Put the preprocessed data at the bottom of the nested processing dict.
+    return initial_processing_dict
+
+
+def rec_processing(individual_dict, parameters, steps, nstep=0, info={}):
 
     if len(steps) == 0:
         return individual_dict
@@ -121,34 +165,51 @@ def rec_processing(individual_dict, parameters, steps, nstep = 0, info = {}):
     for key in individual_dict.keys():
 
         if nstep == 0:
-            info['file_name'] = key
+            info["file_name"] = key
         else:
-            info[steps[nstep-1]] = key
+            info[steps[nstep - 1]] = key
 
-        functions = get_functions_with_args(info = info,
-                                            parameters = parameters['processing'][sub_step],
-                                            step = 'processing',
-                                            step_module = getattr(modules, sub_step))
+        functions = get_functions_with_args(
+            info=info,
+            parameters=parameters["processing"][sub_step],
+            step="processing",
+            step_module=getattr(modules, sub_step),
+        )
 
         for f in functions:
             function, kwargs = f
-            datas = function(data = unnest_data(individual_dict[key]), info = info, **kwargs) # unnest to get the preprocessed data at the bottom of the tree
-            for label, data in datas.items(): # returned data should be a dict with {label : data, [...]}
-                individual_dict[key].update({label : nest_data(steps = steps[nstep+1:], data = data)})
+            datas = function(
+                data=unnest_data(individual_dict[key]), info=info, **kwargs
+            )  # Unnest the preprocessed data from the bottom of the dict.
+            for (
+                label,
+                data,
+            ) in datas.items():
+                individual_dict[key].update(
+                    {label: nest_data(steps=steps[nstep + 1 :], data=data)}
+                )  # Put the processed data at the bottom of the nested dict.
 
-        rec_processing(individual_dict[key], parameters, steps, nstep = nstep+1, info = info)
+        rec_processing(
+            individual_dict[key], parameters, steps, nstep=nstep + 1, info=info
+        )
 
     return individual_dict
+
+
+# %% POSTPROCESSING STEP
+
 
 def postprocessing(processing_dict, parameters):
 
     postprocessing_dict = {}
-    postprocessing_parameters = parameters['postprocessing']
-    
-    functions = get_functions_with_args(info = None,
-                                parameters = parameters['postprocessing'],
-                                step = 'postprocessing',
-                                step_module = modules.postprocessing_functions)
+    postprocessing_parameters = parameters["postprocessing"]
+
+    functions = get_functions_with_args(
+        info=None,
+        parameters=parameters["postprocessing"],
+        step="postprocessing",
+        step_module=modules.postprocessing_functions,
+    )
 
     for f in functions:
         function, kwargs = f
@@ -157,21 +218,27 @@ def postprocessing(processing_dict, parameters):
 
     return postprocessing_dict
 
+
 def postprocessing_mp(processing_dict, parameters):
 
     postprocessing_dict = {}
 
-    functions = get_functions_with_args(info = None,
-                                parameters = parameters['postprocessing'],
-                                step = 'postprocessing',
-                                step_module = modules.postprocessing_functions)
+    functions = get_functions_with_args(
+        info=None,
+        parameters=parameters["postprocessing"],
+        step="postprocessing",
+        step_module=modules.postprocessing_functions,
+    )
 
     q = multiprocessing.Queue()
     mp_list = []
 
     for f in functions:
         function, kwargs = f
-        process = multiprocessing.Process(target=postprocess_mp, args=(q, function, processing_dict, parameters, kwargs))
+        process = multiprocessing.Process(
+            target=postprocess_mp,
+            args=(q, function, processing_dict, parameters, kwargs),
+        )
         process.start()
         mp_list.append(process)
 
@@ -181,34 +248,47 @@ def postprocessing_mp(processing_dict, parameters):
 
     for p in mp_list:
         p.join()
-        #p.close()
 
     return postprocessing_dict
+
 
 def postprocess_mp(q, function, processing_dict, parameters, kwargs):
     data = function(copy.deepcopy(processing_dict), parameters, **kwargs)
     q.put((function, data))
 
+
+# %% HELPER FUNCTIONS
+
+
 def get_functions_with_args(info, parameters, step, step_module):
 
     functions = []
 
-    if step == 'postprocessing':
+    if step == "postprocessing":
         for function_name, kwargs in parameters.items():
             function = getattr(step_module, function_name)
             functions.append((function, kwargs))
         return functions
 
-    options = [opt['PARAMETERS'] for opt in parameters.values() if opt['CONDITION'](info)]
-    if len(options) == 0 : # We don't permit to ignore any file of the query on the preprocessing step, but it's not a problem for the processing step.
-        if step == 'preprocessing':
+    options = [
+        opt["PARAMETERS"] for opt in parameters.values() if opt["CONDITION"](info)
+    ]
+
+    if (
+        len(options) == 0
+    ):  # We don't permit to ignore any file of the query on the preprocessing step, but it's not a problem for the processing step.
+        if step == "preprocessing":
             raise Exception(f"No conditions matching for {info}.")
-        elif step == 'processing': 
+        elif step == "processing":
             warnings.warn(f"No conditions matching for {info}.")
             return []
-    elif len(options) > 1 :
-        raise Exception(f"Conditions should be mutually exclusive. {len(option)} conditions matched for {info}.")
-    else :
+
+    elif len(options) > 1:
+        raise Exception(
+            f"Conditions should be mutually exclusive. {len(option)} conditions matched for {info}."
+        )
+
+    else:
         option = options[0]
 
     for function_name, kwargs in option.items():
@@ -217,15 +297,16 @@ def get_functions_with_args(info, parameters, step, step_module):
 
     return functions
 
+
 def nest_data(steps, data):
     if len(steps) == 0:
         return data
     if len(steps) == 1:
-        return {f"no_{steps[0]}" : data}
-    return {f"no_{steps[0]}" : nest_data(steps[1:], data)}
+        return {f"no_{steps[0]}": data}
+    return {f"no_{steps[0]}": nest_data(steps[1:], data)}
+
 
 def unnest_data(d):
-    print(list(d.keys()))
     if len(d.keys()) > 1:
         raise Exception("There shouldn't be more than one key at this step.")
     value = d[list(d.keys())[0]]
@@ -233,11 +314,19 @@ def unnest_data(d):
         return unnest_data(value)
     return value
 
-if __name__ == "__main__":
-    #multiprocessing.set_start_method('spawn')
-    file_path_list = glob.glob(data_query)
-    processing_dict = main(file_path_list, parameters = PARAMETERS, apply_multiprocessing = False)
-    processing_dict_mp = main(file_path_list, parameters = PARAMETERS, apply_multiprocessing = True)
-    print(processing_dict)
-    print(processing_dict_mp)
 
+if __name__ == "__main__":
+
+    file_path_list = glob.glob(DATA_QUERY)
+
+    preprocessing_dict, processing_dict, postprocessing_dict = main(
+        file_path_list=file_path_list,
+        parameters=PARAMETERS,
+        apply_multiprocessing=False,
+    )
+
+    preprocessing_dict_mp, processing_dict_mp, postprocessing_dict_mp = main(
+        file_path_list=file_path_list,
+        parameters=PARAMETERS,
+        apply_multiprocessing=True,
+    )
--- parameters.py
+++ parameters.py
@@ -1,43 +1,56 @@
-data_query = "DATA/*.fif"
+DATA_QUERY = "DATA/*.fif"
 
-preprocessing_parameters = {'all' : {'CONDITION' : lambda file_path : 'task-sart' in file_path,
-                                           'PARAMETERS' : {'EPICE_minimal_preprocessing' : {'LOW_FREQ' : 0.1,
-                                                                                            'HIGH_FREQ' : 40,
-                                                                                            'NOTCH_FILTER' : 50}
-                                                           }
-                                          },
-                                 'other' : {'CONDITION' : lambda file_path : 'task-sart' not in file_path,
-                                            'PARAMETERS' : {'EPICE_minimal_preprocessing' : {'LOW_FREQ' : 0.1,
-                                                                                            'HIGH_FREQ' : 40,
-                                                                                            'NOTCH_FILTER' : 50}
-                                                            }
-                                            }
-                                 }
+preprocessing_parameters = {
+    "all": {
+        "CONDITION": lambda file_path: "task-sart" in file_path,
+        "PARAMETERS": {
+            "EPICE_minimal_preprocessing": {
+                "LOW_FREQ": 0.1,
+                "HIGH_FREQ": 40,
+                "NOTCH_FILTER": 50,
+            }
+        },
+    },
+    "other": {
+        "CONDITION": lambda file_path: "task-sart" not in file_path,
+        "PARAMETERS": {
+            "EPICE_minimal_preprocessing": {
+                "LOW_FREQ": 0.1,
+                "HIGH_FREQ": 40,
+                "NOTCH_FILTER": 50,
+            }
+        },
+    },
+}
 
-processing_parameters = {'extraction' : {'all' : {'CONDITION' : lambda info : True,
-                                                       'PARAMETERS' : {'subset_epoching' : {'tmin' : -20,
-                                                                                            'tmax' : 0,
-                                                                                            'ptp_threshold' : None,
-                                                                                            'reject_flat' : 1e-6,
-                                                                                            'add_df' : False,
-                                                                                            'labels' : {'probe':'probe'} 
-                                                                                            }
-                                                                       }
-                                                       }
-                                              },
-                              'analysis' : {'epochs' : {'CONDITION' : lambda info : 'epochs' in info['extraction'],
-                                                        'PARAMETERS' : {'psd_data' : {'bp' : False}
-                                                                        }
-                                                        }
-                                            }
-                              }
+processing_parameters = {
+    "extraction": {
+        "all": {
+            "CONDITION": lambda info: True,
+            "PARAMETERS": {
+                "subset_epoching": {
+                    "tmin": -20,
+                    "tmax": 0,
+                    "ptp_threshold": None,
+                    "reject_flat": 1e-6,
+                    "add_df": False,
+                    "labels": {"probe": "probe"},
+                }
+            },
+        }
+    },
+    "analysis": {
+        "epochs": {
+            "CONDITION": lambda info: "epochs" in info["extraction"],
+            "PARAMETERS": {"psd_data": {"bp": False}},
+        }
+    },
+}
 
-postprocesssing_parameters = {'psd_mean' : {},
-                                  'psd_mean' : {},
-                                  'psd_mean' : {}
-                              }
+postprocesssing_parameters = {"psd_mean": {}, "psd_mean": {}, "psd_mean": {}}
 
-PARAMETERS = {'preprocessing' : preprocessing_parameters,
-              'processing' : processing_parameters,
-              'postprocessing' : postprocesssing_parameters}
-
+PARAMETERS = {
+    "preprocessing": preprocessing_parameters,
+    "processing": processing_parameters,
+    "postprocessing": postprocesssing_parameters,
+}
